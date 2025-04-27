1a1ea6176c81ebbeb0bc20fd468fdc1814cd5d18
racha meziane <meziane.rachaa@gmail.com>
2025-04-27 23:12:11 +0200

    version initiale avec main

--- main.cpp
+++ main.cpp
@@ -4,10 +4,27 @@
 #include "training.h"
 #include <random>
 
+constexpr const char* TRAIN_IMAGES = "D:\\mnist\\train-images.idx3-ubyte";
+constexpr const char* TRAIN_LABELS = "D:\\mnist\\train-labels.idx1-ubyte";
+constexpr const char* TEST_IMAGES = "D:\\mnist\\t10k-images.idx3-ubyte";
+constexpr const char* TEST_LABELS = "D:\\mnist\\t10k-labels.idx1-ubyte";
 
-
+constexpr int   EPOCHS = 3;
+constexpr float LR = 0.01f;
 
 int main() {
-return 0; 
+    try {
+        Images Xtr = load_images(TRAIN_IMAGES);
+        Labels Ytr = load_labels(TRAIN_LABELS);
+        Images Xte = load_images(TEST_IMAGES);
+        Labels Yte = load_labels(TEST_LABELS);
 
+        std::mt19937 gen(42);
+        CNN net(LR, gen);
+        train_epoch_loop(net, Xtr, Ytr, Xte, Yte, EPOCHS);
+    }
+    catch (const std::exception& ex) {
+        std::cerr << "ERROR: " << ex.what() << "\n";
+        return 1;
+    }
 }
